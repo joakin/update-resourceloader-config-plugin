b056c714c0faf55bab225302b73d0eb7c5433f8e
joakin <joaquin@chimeces.com>
2015-08-24 15:45:37 +0200

    Properly format UpdateResourceLoaderConfigPlugin

--- index.js
+++ index.js
@@ -7,44 +7,43 @@ var ExtractMFRequiredModules = require('./ExtractMFRequiredModules')
 module.exports = UpdateResourceLoaderConfig
 
 function UpdateResourceLoaderConfig (opts) {
-	opts = opts || {}
+  opts = opts || {}
 
-	this.currentRLInfo = newModuleInfo()
+  this.currentRLInfo = newModuleInfo()
 
-	this.extractI18nKeys = new ExtractI18nKeys({
-		functionName: opts.i18n,
-		onKeys: this.updateEntrypointsRLInfo.bind(this, 'i18n')
-	})
-	this.extractMFModules = new ExtractMFRequiredModules({
-		onModules: this.updateEntrypointsRLInfo.bind(this, 'modules')
-	})
+  this.extractI18nKeys = new ExtractI18nKeys({
+    functionName: opts.i18n,
+    onKeys: this.updateEntrypointsRLInfo.bind(this, 'i18n')
+  })
+  this.extractMFModules = new ExtractMFRequiredModules({
+    onModules: this.updateEntrypointsRLInfo.bind(this, 'modules')
+  })
 }
 
 UpdateResourceLoaderConfig.prototype.updateEntrypointsRLInfo = function (key, data) {
-	this.currentRLInfo[key] = data
+  this.currentRLInfo[key] = data
 }
 
-UpdateResourceLoaderConfig.prototype.apply = function(compiler) {
-	var self = this
-	this.extractI18nKeys.apply(compiler)
-	this.extractMFModules.apply(compiler)
+UpdateResourceLoaderConfig.prototype.apply = function (compiler) {
+  this.extractI18nKeys.apply(compiler)
+  this.extractMFModules.apply(compiler)
 
-	compiler.plugin("done", function() {
-		var modules = parsePluginsToRL(this.currentRLInfo)
-		this.currentRLInfo = newModuleInfo()
-		updateResourceLoaderModules(path.relative('./', compiler.outputPath), modules)
-	}.bind(this))
+  compiler.plugin('done', function () {
+    var modules = parsePluginsToRL(this.currentRLInfo)
+    this.currentRLInfo = newModuleInfo()
+    updateResourceLoaderModules(path.relative('./', compiler.outputPath), modules)
+  }.bind(this))
 }
 
 function newModuleInfo () { return { i18n: null, modules: null } }
 
 function parsePluginsToRL (info) {
-	var allKeys = Object.keys(info.i18n).concat(Object.keys(info.modules))
-	return allKeys.reduce(function (acc, k) {
-		acc[k] = {
-			i18n: info.i18n[k],
-			modules: info.modules[k]
-		}
-		return acc
-	}, {})
+  var allKeys = Object.keys(info.i18n).concat(Object.keys(info.modules))
+  return allKeys.reduce(function (acc, k) {
+    acc[k] = {
+      i18n: info.i18n[k],
+      modules: info.modules[k]
+    }
+    return acc
+  }, {})
 }
